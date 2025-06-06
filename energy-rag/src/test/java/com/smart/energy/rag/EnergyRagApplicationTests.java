e5d787cdd45f78f73472d195daf64cf7aa94acbb
BruceZhang54110 <zhw54110@163.com>
2025-06-06 10:07:40 +0800

    first commit

---
+++ energy-rag/src/test/java/com/smart/energy/rag/EnergyRagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smart.energy.rag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergyRagApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
